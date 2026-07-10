a1e8ac2be6dbdb4e3e8abde850808a733e0e4d60
FahrezaAdam <rezarendra29@gmail.com>
2026-07-10 12:54:42 +0700

    Force direct download for CV via Laravel response()->download()

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,8 @@ use Illuminate\Http\Request;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/download-cv', function () {
+    $file = public_path('CV_Fahreza_Adam_Nuardiansyah.pdf');
+    return response()->download($file, 'CV_Fahreza_Adam_Nuardiansyah.pdf');
+});
